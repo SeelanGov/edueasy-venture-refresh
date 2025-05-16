92d3ac9774e049a57b692ab02e467e6327e5e8a4
SeelanGov <seelan.ube@gmail.com>
2025-05-16 13:54:25 +0200

    fix(forms): improve select and checkbox label visibility in all themes for accessibility

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,15 @@
 export default {
   preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
-  transform: {},
-  globals: {
-    'ts-jest': {
-      useESM: true
-    }
+  transform: {
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      { useESM: true }
+    ]
   },
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
   moduleNameMapper: {
     '^(\.{1,2}/.*)\\.js$': '$1',
   },
 };
+// Cleaned up config: moved ts-jest options into transform, removed deprecated and unknown keys.
